572a7955dcf54106a07c2ca96843883563746abe
NangmanGureum <lastkwak@gmail.com>
2021-08-31 20:50:20 +0900

    Add a first stuff of GUI

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use std::path::Path;
 mod audio;
 mod file_maneger;
 
-fn main() {
+use druid::widget::Label;
+use druid::{AppLauncher, PlatformError, Widget, WindowDesc};
+
+fn main() -> Result<(), PlatformError> {
     // Audio Reading Test
     // let sample_file = file_maneger::File::new("./example_audio.mp3");
     // let sample_audio = audio::AudioFile::new(Path::new(&sample_file.full_name)).unwrap();
@@ -11,4 +14,10 @@ fn main() {
     // File Play Test
     // let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     // audio::AudioFile::play(&sample_audio, &stream_handle);
+    AppLauncher::with_window(WindowDesc::new(main_window())).launch(())?;
+    Ok(())
+}
+
+fn main_window() -> impl Widget<()> {
+    Label::new("Hello world")
 }
